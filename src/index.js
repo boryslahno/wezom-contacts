d5fa8ff7f33ae6401bb4032ea8aea42e431f53ef
boryslahno <borys.lahno@gmail.com>
2021-12-13 12:02:37 +0200

    Changed ConnectedRouter to HashRouter

--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { ConnectedRouter } from 'connected-react-router';
 import { Provider as ReduxProvider } from 'react-redux';
 import { store } from './store';
 import { history } from './store/middleware';
 import './assets/styles/index.scss';
 import 'antd/dist/antd.css';
 import { RoutesSwitch } from './routes';
+import { HashRouter } from 'react-router-dom';
+
 
 ReactDOM.render(
   <ReduxProvider store={store}>
-    <ConnectedRouter history={history}>
+    <HashRouter history={history}>
       <RoutesSwitch />
-    </ConnectedRouter>
+    </HashRouter>
   </ReduxProvider>,
   document.getElementById('root')
 );
